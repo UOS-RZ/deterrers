5684c662a9164bcb4e7ce25489644c533f1cdafe
nwintering <nwintering@uni-osnabrueck.de>
2022-12-13 15:24:55 +0100

    bugfix

--- new-src/deterrers-src/deterrers-app/hostadmin/urls.py
+++ new-src/deterrers-src/deterrers-app/hostadmin/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('hosts/', views.hosts_list_view, name='hosts_list'),
     path('overview/', views.hostadmin_overview_view, name='hostadmin_overview'),
     path('', views.about_view, name='about'),
-    path('greenbone-alert/' view.greenbone_alert, name='greenbone_alert')
+    path('greenbone-alert/', views.greenbone_alert, name='greenbone_alert')
 ]
