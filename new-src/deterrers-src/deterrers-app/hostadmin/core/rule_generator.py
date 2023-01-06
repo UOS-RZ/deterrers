6669577d92fe57f7f2c799e1891c4f689a4f5d04
nwintering <nwintering@uni-osnabrueck.de>
2023-01-06 14:23:53 +0100

    stash

--- new-src/deterrers-src/deterrers-app/hostadmin/core/rule_generator.py
+++ new-src/deterrers-src/deterrers-app/hostadmin/core/rule_generator.py
@@ -1,5 +1,42 @@
+import os
+
+from .host import HostFWContract, HostServiceContract
+
+from django.conf import settings
+
+STATIC_PATH = os.path.join(settings.STATIC_ROOT, 'fw-config-samples')
 
 
-def generate_rule(fw, service_profile):
-     # TODO: implement
-     pass
+def __get_fw_folder(fw : HostFWContract) -> str:
+    match fw:
+        case HostFWContract.UFW:
+            return "/ufw/"
+        case HostFWContract.FIREWALLD:
+            return "/firewalld/"
+        case HostFWContract.NFTABLES:
+            return "/nftables/"
+        case _:
+            raise RuntimeError(f"Invalid firewall program: {fw}")
+
+def __get_profile_folder(service_profile : HostServiceContract) -> str:
+    match service_profile:
+        case HostServiceContract.HTTP:
+            return "/http/"
+        case HostServiceContract.SSH:
+            return "/ssh/"
+        case HostServiceContract.MULTIPURPOSE:
+            return "/open/"
+        case _:
+            raise RuntimeError(f"Invalid service profile: {service_profile}")
+
+
+def generate_rule(fw : HostFWContract, service_profile : HostServiceContract) -> str:
+    # TODO: docu
+    fw_folder = __get_fw_folder(fw)
+    service_profile_folder = __get_profile_folder(service_profile)
+
+    conf_path = os.path.join(STATIC_PATH, fw_folder, service_profile_folder)
+
+    with open(conf_path, "r") as f:
+    conf = f.read()
+    return conf
