b57f8c6335a04452867f997f2daa494e94ac6b80
nwintering <nwintering@uni-osnabrueck.de>
2023-01-06 19:15:27 +0100

    docu

--- new-src/deterrers-src/deterrers-app/hostadmin/core/rule_generator.py
+++ new-src/deterrers-src/deterrers-app/hostadmin/core/rule_generator.py
@@ -8,6 +8,15 @@ STATIC_PATH = os.path.join(settings.STATIC_ROOT, 'fw-config-samples')
 
 
 def __get_fw_folder(fw : HostFWContract) -> str:
+    """
+    Get the folder name corresponding to the firewall programm.
+
+    Args:
+        fw (HostFWContract): Firewall program
+    
+    Raises:
+        RuntimeError: Raised if fw is not supported by HostFWContract.
+    """
     match fw:
         case HostFWContract.UFW:
             return "/ufw/"
@@ -19,6 +28,15 @@ def __get_fw_folder(fw : HostFWContract) -> str:
             raise RuntimeError(f"Invalid firewall program: {fw}")
 
 def __get_profile_folder(service_profile : HostServiceContract) -> str:
+    """
+    Get the folder name corresponding to the service profile.
+
+    Args:
+        service_profile (HostServiceContract): Service profile.
+    
+    Raises:
+        RuntimeError: Raised if service_profile is not supported by HostServiceContract.
+    """
     match service_profile:
         case HostServiceContract.HTTP:
             return "/http/"
@@ -31,12 +49,18 @@ def __get_profile_folder(service_profile : HostServiceContract) -> str:
 
 
 def generate_rule(fw : HostFWContract, service_profile : HostServiceContract) -> str:
-    # TODO: docu
+    """
+    Generate/Suggest a firewall rule for some combination of fw program and service profile.
+
+    Args:
+        fw (HostFWContract): Firewall program.
+        service_profile (HostServiceContract): Service profile.
+    """
     fw_folder = __get_fw_folder(fw)
     service_profile_folder = __get_profile_folder(service_profile)
 
     conf_path = os.path.join(STATIC_PATH, fw_folder, service_profile_folder)
 
     with open(conf_path, "r") as f:
-    conf = f.read()
-    return conf
+        conf = f.read()
+        return conf
