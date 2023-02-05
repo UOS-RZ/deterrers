52619ae69cbabfd3932ff4ab401dc6a028716332
nwintering <nwintering@uni-osnabrueck.de>
2023-02-05 11:14:54 +0100

    .

--- new-src/deterrers-src/deterrers-app/hostadmin/core/rule_generator.py
+++ new-src/deterrers-src/deterrers-app/hostadmin/core/rule_generator.py
@@ -256,13 +256,13 @@ firewall-cmd --zone={CUSTOM_ZONE} --add-rich-rule='rule family={allow_family} so
 
 
     POSTAMBLE = \
-"""
-
-# set custom zone to default zone
-firewall-cmd --set-default-zone={CUSTOM_ZONE}
+f"""
 
 #set the target of custom zone to ALLOW in order to make it default behaviour
-firewall-cmd --zone={CUSTOM_ZONE} --set-target=ACCEPT
+firewall-cmd --permanent --zone={CUSTOM_ZONE} --set-target=ACCEPT
+
+# set custom zone to default zone
+firewall-cmd --permanent --set-default-zone={CUSTOM_ZONE}
 
 # make changes permanent
 firewall-cmd --runtime-to-permanent
