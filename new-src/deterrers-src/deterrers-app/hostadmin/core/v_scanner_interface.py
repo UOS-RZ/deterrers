ff465ff11c36861913cd1b25f373e8a7d7b17019
nwintering <nwintering@uni-osnabrueck.de>
2023-02-21 18:52:03 +0100

    Add new service profile HTTP+SSH and implement notification system. Also make sure IPs are removed from all AddressGroups before they are added to new ones on successful registrations.

--- new-src/deterrers-src/deterrers-app/hostadmin/core/v_scanner_interface.py
+++ new-src/deterrers-src/deterrers-app/hostadmin/core/v_scanner_interface.py
@@ -698,7 +698,7 @@ class GmpVScannerInterface():
                 try:
                     hosts.remove(host_ip)
                 except ValueError:
-                    logger.error(f"Tried to remove host {host_ip} from periodic task with target list {hosts}. Does not exist!")
+                    logger.warning(f"Tried to remove host {host_ip} from periodic task with target list {hosts}. Does not exist!")
                 response = self.gmp.modify_target(
                     new_target_uuid,
                     hosts=hosts,
