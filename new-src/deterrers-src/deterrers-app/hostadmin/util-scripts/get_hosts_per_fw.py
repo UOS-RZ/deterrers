2c649de758e31a85f2ed0629b97adfb3dad1c0bf
nwintering <nwintering@uni-osnabrueck.de>
2023-03-14 16:34:16 +0100

    thesis

---
+++ new-src/deterrers-src/deterrers-app/hostadmin/util-scripts/get_hosts_per_fw.py
@@ -0,0 +1,23 @@
+import getpass
+import logging
+
+from ..core.ipam_api_interface import ProteusIPAMInterface
+from ..core.contracts import HostFWContract
+
+
+if __name__ == '__main__':
+    username = 'nwintering'
+    password = getpass.getpass(f"Password for {username}:")
+
+    fw_host_mapping = {fw : set() for fw in HostFWContract}
+    with ProteusIPAMInterface(username, password, 'proteus.rz.uos.de') as ipam:
+        admin_tag_names = ipam.get_admin_tag_names()
+        for a_tag_name in admin_tag_names:
+            hosts = ipam.get_hosts_of_admin(admin_rz_id=a_tag_name)
+            for host in hosts:
+                fw_host_mapping[host.fw].add(host.ipv4_addr)
+    
+    for fw, hosts in fw_host_mapping.items():
+        print(fw.value)
+        for host in hosts:
+            print(f"  {host}")
