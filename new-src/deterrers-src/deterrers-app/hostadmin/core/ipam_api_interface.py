5779c622888dbf5a374ebb309c484bd0ffb55b6c
Lars Kiesow <lkiesow@uos.de>
2023-04-14 17:44:19 +0200

    Fix tag conflicts
    This patch fixes a bug in handling several tags where the API would
    sometimes complain about already existing tags and sometimes wouldn't.
    
    This will work just fine:
    ```
    ❯ deterrers-cli add --admin lkiesow --admin virtUOS 131.173.20.15
    ```
    
    This will result in a 409 Conflict:
    ```
    ❯ deterrers-cli add --admin virtUOS --admin lkiesow 131.173.20.15
    ```
    
    Worse, this will not only result in a conflict but also leave a half
    added entity around in DETERRERS.
    
    If someone wants a tag to be added it should be fine to accept the
    request and return success if the tag already exists.

--- new-src/deterrers-src/deterrers-app/hostadmin/core/ipam_api_interface.py
+++ new-src/deterrers-src/deterrers-app/hostadmin/core/ipam_api_interface.py
@@ -674,14 +674,15 @@ deterrers_rules={json.dumps([p.to_string() for p in host.host_based_policies])}|
             # get tag object
             tag_id = self.__get_tag_id(tag_name)
 
+            # host host is already tagged, we are done
+            if self.host_is_tagged(host_id, tag_id):
+                return 200
+
             # link tag to host
-            if not self.host_is_tagged(host_id, tag_id):
-                linkentities_params = f"entity1Id={host_id}&entity2Id={tag_id}"
-                linkentities_url = self.main_url + "linkEntities?" + linkentities_params
-                response = requests.put(linkentities_url, headers=self.header, timeout=self.TIMEOUT)
-                return response.status_code
-            else:
-                return 409
+            linkentities_params = f"entity1Id={host_id}&entity2Id={tag_id}"
+            linkentities_url = self.main_url + "linkEntities?" + linkentities_params
+            response = requests.put(linkentities_url, headers=self.header, timeout=self.TIMEOUT)
+            return response.status_code
         except Exception:
             logger.exception("Couldn't add tag to host!")
 
