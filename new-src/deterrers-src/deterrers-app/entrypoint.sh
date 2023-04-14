ec1b59ea8823c91161084f411d75ca489e8d5b48
nwintering <nwintering@uni-osnabrueck.de>
2023-04-14 23:01:52 +0200

    remove cronjob experiments

--- new-src/deterrers-src/deterrers-app/entrypoint.sh
+++ new-src/deterrers-src/deterrers-app/entrypoint.sh
@@ -8,6 +8,6 @@ python manage.py collectstatic --noinput
 python manage.py add_ssh_fingerprints
 python manage.py createsuperuser --noinput || true # suppress error msg that is thrown if superuser already exists
 
-crontab -u ${APP_USER} -l | { cat; echo "*/1 * * * * python ${MICRO_SERVICE}/manage.py commit_to_perimeter_fw"; } | crontab -u ${APP_USER} -
+# crontab -u ${APP_USER} -l | { cat; echo "*/1 * * * * python ${MICRO_SERVICE}/manage.py commit_to_perimeter_fw"; } | crontab -u ${APP_USER} -
                     
 exec "$@"
