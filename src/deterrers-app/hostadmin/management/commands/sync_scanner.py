c131cdc0549e8b0bdb3a3779b5c56e00a02e957c
nwintering <nwintering@uni-osnabrueck.de>
2023-06-08 15:02:21 +0200

    sync scanner management command

---
+++ src/deterrers-app/hostadmin/management/commands/sync_scanner.py
@@ -0,0 +1,154 @@
+from django.core.management.base import BaseCommand, CommandError
+import os
+import argparse
+import ipaddress
+import logging
+
+from django.conf import settings
+
+from hostadmin.core.ipam_api_interface import ProteusIPAMInterface
+from hostadmin.core.v_scanner_interface import GmpVScannerInterface
+from hostadmin.core.contracts import HostStatusContract, HostServiceContract
+from hostadmin.core.host import MyHost
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = 'Compares data in IPAM with data in vulnerability scanner.'
+
+    sync = False
+
+    def add_arguments(self, parser):
+        parser.add_argument('-s', '--sync', action='store_true', help='Indicates whether to actually update the scanner configuration')
+
+    def __add_ip(self, scanner : GmpVScannerInterface, ipv4 : str):
+        logger.warning("IP %s is missing in scanner", str(ipv4))
+        if self.sync:
+            scanner.add_host_to_periodic_scans(ipv4, '')
+
+    def __rmv_ip(self, scanner : GmpVScannerInterface, ipv4 : str):
+        logger.warning("IP %s is wrongfully present in scanner", str(ipv4))
+        if self.sync:
+            scanner.remove_host_from_periodic_scans(ipv4)
+
+    def __sync_host_online(
+            self,
+            host :  MyHost,
+            ipam : ProteusIPAMInterface,
+            scanner : GmpVScannerInterface, 
+            v_scanner_hosts : set):
+        
+        ipv4 = str(host.ipv4_addr)
+        if ipv4 not in v_scanner_hosts:
+            self.__add_ip(scanner, ipv4)
+
+    def __sync_host_blocked(
+            self,
+            host :  MyHost,
+            ipam : ProteusIPAMInterface,
+            scanner : GmpVScannerInterface,
+            v_scanner_hosts : set):
+        
+        ipv4 = str(host.ipv4_addr)
+        if ipv4 in v_scanner_hosts:
+            self.__rmv_ip(scanner, ipv4)
+
+    def __sync_host_under_review(self, host : MyHost):
+        logger.info("Host under review: %s", str(host.ipv4_addr))
+
+    def handle(self, *args, **options):
+        # quick sanity check if service profiles are still up-to-date
+        if not {sp for sp in HostServiceContract} == {HostServiceContract.EMPTY, HostServiceContract.HTTP, HostServiceContract.SSH, HostServiceContract.HTTP_SSH, HostServiceContract.MULTIPURPOSE}:
+            logger.error("Service Profiles not up-to-date!")
+            exit()
+
+        self.sync = options.get('sync', None)
+
+        while True:
+            ipam_username = os.environ.get('IPAM_USERNAME')
+            ipam_password = os.environ.get('IPAM_SECRET_KEY',)
+            ipam_url = os.environ.get('IPAM_URL')
+            # ipam_username = settings.IPAM_USERNAME
+            # ipam_password = settings.IPAM_SECRET_KEY
+            # ipam_url = settings.IPAM_URL
+            with ProteusIPAMInterface(ipam_username, ipam_password, ipam_url) as ipam:
+                if not ipam.enter_ok:
+                    continue
+                
+                while True:
+                    v_scanner_username = os.environ.get('V_SCANNER_USERNAME')
+                    v_scanner_password = os.environ.get('V_SCANNER_SECRET_KEY')
+                    v_scanner_url = os.environ.get('V_SCANNER_URL')
+                    # v_scanner_username = settings.V_SCANNER_USERNAME
+                    # v_scanner_password = settings.V_SCANNER_SECRET_KEY
+                    # v_scanner_url = settings.V_SCANNER_URL
+                    with GmpVScannerInterface(v_scanner_username, v_scanner_password, v_scanner_url) as scanner:
+                        if not scanner.enter_ok:
+                            continue
+
+                        #### GET DATA ####
+
+                        # get all hosts in IPAM
+                        logger.info("Get assets from IPAM!")
+                        ipam_hosts_total = {}
+                        admin_tag_names = ipam.get_admin_tag_names()
+                        for a_tag_name in admin_tag_names:
+                            hosts = ipam.get_hosts_of_admin(admin_rz_id=a_tag_name)
+                            for host in hosts:
+                                ipam_hosts_total[str(host.ipv4_addr)] = host
+                        
+
+                        logger.info('Get assets in periodic scan!')
+                        try:
+                            v_scanner_hosts = set()
+                            # get periodic task info, get target info, get hosts
+                            filter_str = f'"{scanner.PERIODIC_TASK_NAME}" rows=-1 first=1'
+                            response = scanner.gmp.get_tasks(filter_string=filter_str)
+                            response_status = int(response.xpath('@status')[0])
+                            if response_status != 200:
+                                raise RuntimeError(f"Couldn't get tasks! Status: {response_status}")
+                            
+                            # if stash target exists use it, else use default target
+                            target_uuid = scanner._GmpVScannerInterface__get_target_id(scanner.PERIODIC_TASK_STASH_TARGET_NAME)
+                            if not target_uuid:
+                                target_uuid = response.xpath('//target/@id')[0]
+
+                            response = scanner.gmp.get_target(target_uuid)
+                            response_status = int(response.xpath('@status')[0])
+                            if response_status != 200:
+                                raise RuntimeError(f"Couldn't get target! Status: {response_status}")
+                            hosts_str = response.xpath('//hosts')[0].text
+                            v_scanner_hosts = {h.strip() for h in hosts_str.split(',')}
+                        except:
+                            logger.exception("")
+
+
+                        for ipv4, host in ipam_hosts_total.items():
+
+                            match host.status:
+                                case HostStatusContract.ONLINE:
+                                    self.__sync_host_online(host, ipam, scanner, v_scanner_hosts)
+                                case HostStatusContract.UNDER_REVIEW:
+                                    self.__sync_host_under_review(host)
+                                case HostStatusContract.BLOCKED | HostStatusContract.UNREGISTERED:
+                                    self.__sync_host_blocked(host, ipam, scanner, v_scanner_hosts)
+                                case _:
+                                    logger.warning("Invalid host status: %s", str(host.status))
+                    
+                    return
+
+
+if __name__ == "__main__":
+    # set logger for manual executions
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sync', action='store_true', help='Indicates whether to actually update the Scanner configuration')
+    args = parser.parse_args()
+
+    c = Command()
+    c.handle(sync=args.sync)
