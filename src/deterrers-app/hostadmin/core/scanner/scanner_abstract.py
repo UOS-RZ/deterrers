7d762a31be27e85ce4f1f3cd0945c7e4ed17bc21
nwintering <nwintering@uni-osnabrueck.de>
2023-07-24 17:25:51 +0200

    Refactoring vulnerability scanner wrapper and abstract class

--- src/deterrers-app/hostadmin/core/scanner/scanner_abstract.py
+++ src/deterrers-app/hostadmin/core/scanner/scanner_abstract.py
@@ -1,1 +1,86 @@
-# TODO
+from abc import ABC, abstractmethod
+
+
+class ScannerAbstract(ABC):
+    """
+    Abstract class for implementation of vulnerability scanner wrappers.
+    """
+
+    @abstractmethod
+    def create_ordinary_scan(
+        self,
+        host_ip: str,
+        alert_dest_url: str
+    ) -> tuple[str, str, str, str]:
+        pass
+
+    @abstractmethod
+    def create_registration_scan(
+        self,
+        host_ip: str,
+        alert_dest_url: str
+    ) -> tuple[str, str, str, str]:
+        pass
+
+    @abstractmethod
+    def create_periodic_scan(
+        self,
+        host_ip: str,
+        alert_dest_url: str,
+        schedule_freq: str
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def add_host_to_periodic_scans(
+        self,
+        host_ip: str,
+        alert_dest_url: str
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def remove_host_from_periodic_scans(
+        self,
+        host_ip: str
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def update_periodic_scan_target(
+        self
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def clean_up_scan_objects(
+        self,
+        target_uuid: str,
+        task_uuid: str,
+        report_uuid: str,
+        alert_uuid: str | list[str]
+    ):
+        pass
+
+    @abstractmethod
+    def get_latest_report_uuid(
+        self,
+        task_uuid: str
+    ) -> str | None:
+        pass
+
+    @abstractmethod
+    def extract_report_data(
+        self,
+        report_uuid: str,
+        min_qod: int
+    ) -> tuple[str, str, dict]:
+        pass
+
+    @abstractmethod
+    def get_report_html(
+        self,
+        report_uuid: str,
+        min_qod: int
+    ) -> str:
+        pass
