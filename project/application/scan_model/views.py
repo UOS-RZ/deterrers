c2c75aec4a63e93757f92a664674261f4b57f2c2
jcapp4 <jcappellani@uni-os.de>
2024-11-25 17:48:38 +0100

    small fix in scan_model.models

--- project/application/scan_model/views.py
+++ project/application/scan_model/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import HttpResponse
+from django.http import Http404,HttpResponse
 from scan_model.models import Scan, Host_scan
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def testview(request):
     g = Host_scan.objects.all()
     s = g[0].last_scan.report_html 
     return HttpResponse(s,200)
+
+# def testview(request):
+#     try:
+#         host = Host_scan.objects.get(entity_id = )
+#     except ObjectDoesNotExist:
+#         new_host = Host_scan( entity_id = )
+#         new_host.save()
+#         host = Host_scan.objects.get(entity_id = )
+
+#     prev_scan = host.last_scan
+#     new_scan = Scan(report_html = "this is a test",previous_scan = prev_scan)
+#     new_scan.save()
+#     host.last_scan = new_scan
+
+#     return HttpResponse(host.last_scan.report_html,200)
