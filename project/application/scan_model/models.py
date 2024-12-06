9b129ee4dc9aad5a9c3bcdcd78b263e726906c9c
jcapp4 <jcappellani@uni-osnabrueck.de>
2024-12-06 11:27:08 +0100

    reworked scan_model models

--- project/application/scan_model/models.py
+++ project/application/scan_model/models.py
@@ -1,13 +1,32 @@
 from django.db import models
+import json
 
 # Create your models here.
 
-class Scan(models.Model):
-    report_html = models.TextField()
-    previous_scan = models.ForeignKey('self',models.SET_NULL,null=True)
+class Vulnerability(models.Model):
+    uuid = models.CharField(max_length= 128)
+    host_ipv4 = models.CharField(max_length=32)
+    port = models.CharField(max_length=16)
+    proto = models.TextField()
+    hostname = models.TextField()
+    nvt_name = models.TextField()
+    nvt_oid = models.TextField()
+    qod = models.IntegerField()
+    cvss_version = models.IntegerField()
+    cvss_base_score = models.FloatField()
+    cvss_base_vector = models.TextField()
+    description = models.TextField()
+    refs = models.TextField() #json
+    overrides = models.TextField() #json
+    date_time = models.TextField()
+    task_id = models.CharField(max_length= 128)
+    report_id = models.CharField(max_length= 128)
 
-class Host_scan(models.Model):
-    entity_id = models.IntegerField()
-    last_scan = models.ForeignKey(Scan,on_delete=models.SET_NULL,null=True)
-    
+class Scan_report(models.Model):
+    report_xml = models.TextField()
+    report_id = models.IntegerField(primary_key=True)
 
+class Host_Silenced_Vulnerabilities(models.Model):
+    entity_id = models.IntegerField(primary_key=True)
+    uuids = models.TextField #json
+    host_ipv4 = models.CharField(max_length=128)
--- project/application/scan_model/views.py
+++ project/application/scan_model/views.py
@@ -1,24 +1,15 @@
 from django.http import Http404,HttpResponse
-from scan_model.models import Scan, Host_scan
+from scan_model.models import Scan_report
 from django.core.exceptions import ObjectDoesNotExist
-
+import os
+from django.shortcuts import get_object_or_404,render
 # Create your views here.
 def testview(request):
-    g = Host_scan.objects.all()
-    s = g[0].last_scan.report_html 
-    return HttpResponse(s,200)
+    
+    context = {
+        "user.get_username ": "test"
 
-# def testview(request):
-#     try:
-#         host = Host_scan.objects.get(entity_id = )
-#     except ObjectDoesNotExist:
-#         new_host = Host_scan( entity_id = )
-#         new_host.save()
-#         host = Host_scan.objects.get(entity_id = )
-
-#     prev_scan = host.last_scan
-#     new_scan = Scan(report_html = "this is a test",previous_scan = prev_scan)
-#     new_scan.save()
-#     host.last_scan = new_scan
-
-#     return HttpResponse(host.last_scan.report_html,200)
+    }
+    return render(request,'base_generic.html',context)
+    
+  
